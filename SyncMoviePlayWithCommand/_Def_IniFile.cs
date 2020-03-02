fad9acbb50c422e4faf88274651dfd2540474553
Teppei Kuroda <niamugi@gmail.com>
2020-03-02 17:42:48 +0900

    コマンドありの画面をバックアップ

---
+++ SyncMoviePlayWithCommand/_Def_IniFile.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ns_IniFile
+{
+    class SettingIni
+    {
+        public enum IniFileID
+        {
+            NONE,
+            USB = 1,        //ポート名,ボーレート,データビット
+            DISPLAY = 2,    //ディスプレイごとの動画
+            OPTION = 3,     //画面の結合(1,2,3,4,横,縦) true or false
+            OFFSET = 4,      //送信オフセット
+            TURMINAL,
+        }
+
+
+        private string _Data;
+        public string Data { get { return _Data; } private set { _Data = value; } }
+
+        private string filePath;
+
+        //コンストラクター
+        public SettingIni()
+        {
+            ns_Define.Define _def = new ns_Define.Define("sozai", true);
+            filePath = _def.FileFolderPath + "Setting.ini";
+        }
+
+        /// <summary>
+        /// Iniファイルから取得
+        /// </summary>
+        /// <param name="path">
+        /// </param>
+        public void Func_getIni(IniFileID path, out string[] GetData)
+        {
+            var obj = new SettingIni();
+            int p = (int)path;
+            obj.ParseFromIni(p.ToString("00"), filePath);
+            GetData = obj.Data.Split(',');
+        }
+
+        /// <summary>
+        /// Iniファイルへ反映 
+        /// </summary>
+        /// <param name="path">
+        /// </param>
+        /// <param name="s">反映する値</param>
+        public void Func_SetIni(IniFileID path, string s)
+        {
+            var obj = new SettingIni();
+            int p = (int)path;
+            obj.Data = s;
+            obj.ExportToIni(p.ToString("00"), filePath);
+        }
+
+    }
+
+    static class IniFileHelper
+    {
+        [DllImport("KERNEL32.DLL")]
+        public static extern uint GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
+
+        [DllImport("KERNEL32.DLL")]
+        public static extern uint GetPrivateProfileInt(string lpAppName, string lpKeyName, int nDefault, string lpFileName);
+
+        [DllImport("KERNEL32.DLL")]
+        public static extern uint WritePrivateProfileString(string lpAppName, string lpKeyName, string lpString, string lpFileName);
+
+        public static void ParseFromIni<T>(this T self, string section, string filepath)
+        {
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                if (prop.PropertyType == typeof(int))
+                {
+                    prop.SetValue(self, (int)GetPrivateProfileInt(section, prop.Name, 0, Path.GetFullPath(filepath)));
+                }
+                else if (prop.PropertyType == typeof(uint))
+                {
+                    prop.SetValue(self, GetPrivateProfileInt(section, prop.Name, 0, Path.GetFullPath(filepath)));
+                }
+                else
+                {
+                    var sb = new StringBuilder(1024);
+                    GetPrivateProfileString(section, prop.Name, string.Empty, sb, (uint)sb.Capacity, Path.GetFullPath(filepath));
+                    prop.SetValue(self, sb.ToString());
+                }
+            }
+        }
+
+        public static void ExportToIni<T>(this T self, string section, string filepath)
+        {
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                WritePrivateProfileString(section, prop.Name, prop.GetValue(self).ToString(), Path.GetFullPath(filepath));
+            }
+        }
+    }
+}
---
+++ SyncMoviePlayWithCommand/main.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Common
+{
+    public class main
+    {
+        public const string START1 = "COM_START1";
+        public const string START2 = "COM_START2";
+        public const string START3 = "COM_START3";
+        public const string START4 = "COM_START4";
+        public const string START5 = "COM_START5";
+        public const string START6 = "COM_START6";
+        public const string START7 = "COM_START7";
+        public const string START8 = "COM_START8";
+        public const string STOP = "COM_STOP";
+        public const string INIT = "COM_INIT";
+
+        public int joinDisplay_no;
+        public int[] joinDisplay_pos = new int[2];
+        public int[] joinDisplay_size = new int[2];
+        
+        public bool dispReady;  //true…準備完了、false…何かしら問題あり
+        public int _dispCnt;
+        public DISPLAY[] dispList;
+
+        //再生開始時に送信するコマンド
+        public string sendStartCmd = string.Empty;
+        //再生終了時に送信するコマンド
+        public string sendEndCmd = string.Empty;
+        //リピートの指定
+        public bool repeat = false;
+        //再生タイミングのオフセット
+        public int sendOfs = 0;
+
+        public class DISPLAY
+        {
+            public string _name = string.Empty;
+            public int _no = 0;
+            public int[] _pos = new int[2];     //左上の座標
+            public int[] _size = new int[2];    //幅・高さ
+            public string _movieName = string.Empty;
+            public string _moviePath = string.Empty;
+            public bool err = true;
+            public string errMsg = string.Empty;
+
+        }
+
+
+
+        public main()
+        {
+            dispReady = false;
+        }
+
+        public string SetMoviePath(int i,string s)
+        {   
+            
+            if (i >= _dispCnt) return string.Empty;
+
+
+            //動画のチェック&セット
+            dispList[i].err = !chkMovieExist(i,s);
+
+            if (dispList[i].err) return string.Empty;
+            return dispList[i]._movieName;
+        }
+        
+        public bool chkSendTime(string s)
+        {
+            try
+            {
+                int num = 0;
+                sendOfs = 0;
+                //入力値が数値であるかチェック、数値の場合保存する
+                if (int.TryParse(s, out num))
+                {
+                    sendOfs = num;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            //合格
+            return true;
+        }
+
+        /// <summary>
+        /// 動画の存在チェック
+        /// </summary>
+        /// <returns></returns>
+        bool chkMovieExist(int i, String _path)
+        {
+            bool sw = false;
+            dispList[i]._movieName = string.Empty;
+            dispList[i]._moviePath = string.Empty;
+            dispList[i].errMsg = "[" + i + "]準備完了 ";
+            try
+            {
+                FileInfo _info = new FileInfo(@_path);
+                string _f = _info.Name;
+                DirectoryInfo _d = _info.Directory;
+
+                //ファイルの存在をチェック
+                if (!_info.Exists)
+                {
+                    dispList[i].errMsg = "[" + i + "]指定したファイルは存在しません。";
+                    return sw;
+                }
+
+                //ファイルが動画かチェック
+                if (!chkFile(_f))
+                {
+                    dispList[i].errMsg = "[" + i + "]" + _f + "は再生できません。";
+                    return sw;
+                }
+
+                //合格
+                dispList[i]._movieName = _info.Name;
+                dispList[i]._moviePath = _info.FullName;
+                dispList[i].errMsg += _info.Name;
+                sw = true;
+
+//                MessageBox.Show( " [info.Name]" + _info.Name + "\n" + " [DirectoryInfo]" + _d.FullName + "\n" + "[movie_path]" + movie_path + "\n", "TEST");
+
+            }
+            catch (Exception e)
+            {
+                dispList[i].errMsg = "[" + i + "]ファイル名に誤りがあります。";
+                //MessageBox.Show(_path + " [ERR]" + e.Message, "TEST");
+            }
+
+
+            return sw;
+        }
+
+        /// <summary>
+        /// 指定したファイルが動画であるかチェックする
+        /// </summary>
+        /// <param name="_file"></param>
+        /// <returns></returns>
+        bool chkFile(string _file)
+        {
+            bool sw = true;
+
+            if (_file.IndexOf(".mov") > 0) return sw;
+            if (_file.IndexOf(".wmv") > 0) return sw;
+
+            sw = false;
+            return sw;
+        }
+
+        /// <summary>
+        /// 準備完了有無のチェック　＆　準備出来ているかメッセージを返す
+        /// </summary>
+        /// <returns></returns>
+        public string JdgReady()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            dispReady = false;
+            //一つでもムービーがセットされていたら準備完了とする。
+            for(int i=0; i < _dispCnt; i++)
+            {
+                if (!dispList[i].err) dispReady = true;
+
+                sb.Append(dispList[i].errMsg);
+                if (i != _dispCnt - 1) sb.Append("\n");
+            }
+
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// ディスプレイリストを初期化する
+        /// </summary>
+        public void InitdispList()
+        {
+            sendStartCmd = string.Empty;
+            repeat = false;
+            sendOfs = 0;
+
+            _dispCnt = Screen.AllScreens.Length;
+            dispList = new DISPLAY[_dispCnt];
+            int i = 0;
+            foreach(Screen item in Screen.AllScreens)
+            {
+                dispList[i] = new DISPLAY();
+                dispList[i]._name = item.DeviceName;
+                dispList[i]._no = i;
+                dispList[i]._pos[0] = item.Bounds.X;
+                dispList[i]._pos[1] = item.Bounds.Y;
+                dispList[i]._size[0] = item.Bounds.Width;
+                dispList[i]._size[1] = item.Bounds.Height;
+                dispList[i]._moviePath = string.Empty;
+                dispList[i].err = true;
+                dispList[i].errMsg = "[" + i + "]設定無し";
+                
+                i++;
+            }
+
+        }
+
+        public string DebugdispList(int i)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            if ( i >= _dispCnt)
+            {
+                sb.Append("[");
+                sb.Append(i);
+                sb.Append("]");
+                sb.Append("ディスプレイ無し");
+
+                return sb.ToString();
+            }
+
+            sb.Append("[");
+            sb.Append(dispList[i]._no);
+            sb.Append("]");
+//            sb.Append(" Name:");
+            sb.Append(dispList[i]._name);
+            sb.Append(" Size:");
+            sb.Append(dispList[i]._size[0]);
+            sb.Append(",");
+            sb.Append(dispList[i]._size[1]);
+            sb.Append(" Pos:(");
+            sb.Append(dispList[i]._pos[0]);
+            sb.Append(",");
+            sb.Append(dispList[i]._pos[1]);
+            sb.Append(")");
+
+            return sb.ToString();
+        }
+    }
+}
